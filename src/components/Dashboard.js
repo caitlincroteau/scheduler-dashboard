64bc770cf8b686f4a624194aa85daec5835dee2d
Caitlin Croteau <cjcroteau@gmail.com>
2022-08-11 22:59:45 -0700

    update selectPanel function, add functions to update local storage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,21 +29,44 @@ const data = [
 class Dashboard extends Component {
   state = { 
     loading: false,
+    focused: null
   };
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if(this.state.loading) {
       return <Loading />
     }
 
-    const panels = data.map(panel => (
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
         value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
       />
     ));
 
